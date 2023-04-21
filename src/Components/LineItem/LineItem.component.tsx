30ceedd312a1116c3a3d7fcbe5b25c5cf02de564
staffloss1 <130788914+staffloss1@users.noreply.github.com>
2023-04-21 16:18:44 -0400

    Add files via upload

--- src/Components/LineItem/LineItem.component.tsx
+++ src/Components/LineItem/LineItem.component.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ILineItem from "../../DTOs/LineItem.dto";
 import { Button, TableCell, TableRow, TextField } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete'
+
 
 export interface ILineItemProps{
     item:ILineItem,
@@ -16,14 +19,14 @@ export default class LineItem extends React.Component<ILineItemProps> {
                 <TableCell >${this.props.item.LaborCost}</TableCell>
                 <TableCell >${this.props.item.MaterialCost}</TableCell>
                 <TableCell >${this.props.item.InclusiveCost}</TableCell>
-                <TableCell >${this.props.item.Units}</TableCell>
-                <TableCell >{this.props.item.TotalCost} </TableCell>
-                <TableCell >
+                <TableCell >{this.props.item.Units}</TableCell>
+                <TableCell >${this.props.item.TotalCost.toFixed(2)} </TableCell>
+                <TableCell style={{display:"flex"}}>
                     <Button>
-                        
+                        <EditIcon></EditIcon>
                     </Button>    
                     <Button>
-                        
+                        <DeleteIcon></DeleteIcon>
                     </Button>    
                 </TableCell>
         </TableRow>
